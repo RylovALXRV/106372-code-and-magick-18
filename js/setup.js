df3f5a05268d1b7b4ba9dabe28d013471446c829
Алексей <rylovalex2108l@mail.ru>
2019-09-25 15:14:33 +0700

    Выполнил задание №7. Учебный проект: одеть Надежду

--- js/setup.js
+++ js/setup.js
@@ -6,7 +6,8 @@ var wizardParams = {
   LAST_NAMES: ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'],
   COAT_COLORS: ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)',
     'rgb(215, 210, 55)', 'rgb(0, 0, 0)'],
-  EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green']
+  EYES_COLORS: ['black', 'red', 'blue', 'yellow', 'green'],
+  FIREBALL_COLORS: ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848']
 };
 
 var setupElement = document.querySelector('.setup');
@@ -67,6 +68,80 @@ var showSimilarWizards = function () {
   setupSimilarElement.classList.remove('hidden');
 };
 
-setupElement.classList.remove('hidden');
-
 showSimilarWizards();
+
+/* ----- Учебный проект: одеть Надежду ----- */
+
+var KeyCode = {
+  ENTER: 13,
+  ESC: 27
+};
+
+var buttonCloseElement = setupElement.querySelector('.setup-close');
+var fireballInputElement = setupElement.querySelector('input[name="fireball-color"]');
+var iconOpenElement = document.querySelector('.setup-open-icon');
+var userNameInputElement = setupElement.querySelector('.setup-user-name');
+var wizardCoatElement = setupElement.querySelector('.setup-wizard .wizard-coat');
+var wizardEyesElement = setupElement.querySelector('.setup-wizard .wizard-eyes');
+var wizardFireballElement = setupElement.querySelector('.setup-fireball');
+
+var popupCloseClickHandler = function (evt) {
+  if (evt.keyCode === KeyCode.ESC && evt.target !== userNameInputElement) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setupElement.classList.remove('hidden');
+
+  document.addEventListener('keydown', popupCloseClickHandler);
+};
+
+var closePopup = function () {
+  setupElement.classList.add('hidden');
+
+  document.removeEventListener('keydown', popupCloseClickHandler);
+};
+
+var setColorElement = function (target, feature, selector) {
+  target.style.fill = getRandomElement(feature);
+
+  setupElement.querySelector(selector).value = target.style.fill;
+};
+
+var playerClickHandler = function (evt) {
+  switch (evt.target) {
+    case wizardCoatElement:
+      setColorElement(evt.target, wizardParams.COAT_COLORS, 'input[name="coat-color"]');
+      return;
+    case wizardEyesElement:
+      setColorElement(evt.target, wizardParams.EYES_COLORS, 'input[name="eyes-color"]');
+      return;
+    case wizardFireballElement:
+      fireballInputElement.value = getRandomElement(wizardParams.FIREBALL_COLORS);
+      evt.target.style.backgroundColor = fireballInputElement.value;
+      return;
+  }
+};
+
+iconOpenElement.addEventListener('click', function () {
+  openPopup();
+});
+
+iconOpenElement.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === KeyCode.ENTER) {
+    openPopup();
+  }
+});
+
+buttonCloseElement.addEventListener('click', function () {
+  closePopup();
+});
+
+buttonCloseElement.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === KeyCode.ENTER) {
+    closePopup();
+  }
+});
+
+document.querySelector('.setup-player').addEventListener('click', playerClickHandler);
